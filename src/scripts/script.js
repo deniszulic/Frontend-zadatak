1f24aa49c767371669ed78f2777cfb43adcd3008
deniszulic <35138382+deniszulic@users.noreply.github.com>
2021-11-09 12:14:53 +0100

    folder

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,13 @@
 var slike1 = [
-  "slike/slider-image-1.jpg",
-  "slike/slider-image-2.jpg",
-  "slike/slider-image-3.jpg",
-  "slike/slider-image-4.jpg",
-  "slike/slider-image-5.jpg",
-  "slike/slider-image-6.jpg",
-  "slike/slider-image-7.jpg",
-  "slike/slider-image-8.jpg",
-  "slike/slider-image-9.jpg",
+  "assets/slider-image-1.jpg",
+  "assets/slider-image-2.jpg",
+  "assets/slider-image-3.jpg",
+  "assets/slider-image-4.jpg",
+  "assets/slider-image-5.jpg",
+  "assets/slider-image-6.jpg",
+  "assets/slider-image-7.jpg",
+  "assets/slider-image-8.jpg",
+  "assets/slider-image-9.jpg",
 ];
 var index = 6;
 var index1 = 5;
@@ -20,7 +20,7 @@ var first = () => {
     $(".toplist").children().first().fadeOut();
     setTimeout(() => {
       $(".toplist").children().first().remove();
-      $(`<img src=slike/slider-image-${index}.jpg>`).appendTo(".toplist");
+      $(`<img src=assets/slider-image-${index}.jpg>`).appendTo(".toplist");
       index++;
     }, 500);
   } else {
@@ -28,7 +28,7 @@ var first = () => {
     $(".toplist").children().first().fadeOut();
     setTimeout(() => {
       $(".toplist").children().first().remove();
-      $(`<img src=slike/slider-image-${index}.jpg>`).appendTo(".toplist");
+      $(`<img src=assets/slider-image-${index}.jpg>`).appendTo(".toplist");
       index++;
     }, 500);
   }
@@ -38,7 +38,7 @@ var second = () => {
     $(".bottomlist").children().first().fadeOut();
     setTimeout(() => {
       $(".bottomlist").children().first().remove();
-      $(`<img src=slike/slider-image-${index1}.jpg>`).appendTo(".bottomlist");
+      $(`<img src=assets/slider-image-${index1}.jpg>`).appendTo(".bottomlist");
       index1--;
     }, 500);
   } else {
@@ -46,7 +46,7 @@ var second = () => {
     $(".bottomlist").children().first().fadeOut();
     setTimeout(() => {
       $(".bottomlist").children().first().remove();
-      $(`<img src=slike/slider-image-${index1}.jpg>`).appendTo(".bottomlist");
+      $(`<img src=assets/slider-image-${index1}.jpg>`).appendTo(".bottomlist");
       index1--;
     }, 500);
   }
@@ -56,7 +56,7 @@ var third = () => {
     $(".toplist").children().last().fadeOut();
     setTimeout(() => {
       $(".toplist").children().last().remove();
-      $(`<img src=slike/slider-image-${index}.jpg>`).prependTo(".toplist");
+      $(`<img src=assets/slider-image-${index}.jpg>`).prependTo(".toplist");
       index--;
     }, 500);
   } else {
@@ -64,7 +64,7 @@ var third = () => {
     $(".toplist").children().last().fadeOut();
     setTimeout(() => {
       $(".toplist").children().last().remove();
-      $(`<img src=slike/slider-image-${index}.jpg>`).prependTo(".toplist");
+      $(`<img src=assets/slider-image-${index}.jpg>`).prependTo(".toplist");
       index--;
     }, 500);
   }
@@ -74,7 +74,7 @@ var fourth = () => {
     $(".bottomlist").children().last().fadeOut();
     setTimeout(() => {
       $(".bottomlist").children().last().remove();
-      $(`<img src=slike/slider-image-${index1}.jpg>`).prependTo(".bottomlist");
+      $(`<img src=assets/slider-image-${index1}.jpg>`).prependTo(".bottomlist");
       index1--;
     }, 500);
   } else {
@@ -82,7 +82,7 @@ var fourth = () => {
     $(".bottomlist").children().first().fadeOut();
     setTimeout(() => {
       $(".bottomlist").children().first().remove();
-      $(`<img src=slike/slider-image-${index1}.jpg>`).prependTo(".bottomlist");
+      $(`<img src=assets/slider-image-${index1}.jpg>`).prependTo(".bottomlist");
       index1--;
     }, 500);
   }
