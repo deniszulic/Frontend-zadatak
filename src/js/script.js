af69a95af2ebdec6ce1b523bc1bef8dfeece6f69
deniszulic <35138382+deniszulic@users.noreply.github.com>
2021-11-23 15:51:02 +0100

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -13,7 +13,8 @@ for (let i of [...Array(4).keys()]) {
 $("#container-first-button").click(() => {
   $("#container-first-button").attr("disabled", true);
   $("#container-second-button").attr("disabled", true);
-  $(".container-top").children().eq(4).hide('slow', () => {
+  let width_top=$(".container-top").children().eq(4).width();
+  $(".container-top").children().eq(4).animate({width:-width_top}, 'slow',() => {
     $(".container-top").find(":last-child").remove().prependTo(".container-top");
     $(".container-top").find(":first-child").attr("style", "display:block");
     $(".container-top").children().eq(5).attr("style", "display:none")
@@ -21,7 +22,8 @@ $("#container-first-button").click(() => {
     $("#container-second-button").attr("disabled", false);
   })
 
-  $(".container-bottomlist").children().eq(3).hide('slow', () => {
+  let width_bottom=$(".container-bottomlist").children().eq(3).width();
+  $(".container-bottomlist").children().eq(3).animate({width:-width_bottom}, 'slow', () => {
     $(".container-bottomlist").find(":last-child").remove().prependTo(".container-bottomlist");
     $(".container-bottomlist").find(":first-child").attr("style", "display:block");
     $(".container-bottomlist").children().eq(4).attr("style", "display:none")
@@ -30,7 +32,7 @@ $("#container-first-button").click(() => {
 $("#container-second-button").click(() => {
   $("#container-first-button").attr("disabled", true);
   $("#container-second-button").attr("disabled", true);
-  $(".container-top").find(":first-child").hide('slow', () => {
+  $(".container-top").find(":first-child").animate({width:0}, 'slow',() => {
     $(".container-top").find(":first-child").remove().appendTo(".container-top");
     $(".container-top").find(":last-child").attr("style", "display:none");
     $(".container-top").children().eq(4).attr("style", "display:block")
@@ -38,7 +40,7 @@ $("#container-second-button").click(() => {
     $("#container-second-button").attr("disabled", false);
   })
 
-  $(".container-bottomlist").find(":first-child").hide('slow', () => {
+  $(".container-bottomlist").find(":first-child").animate({width:0}, 'slow',() => {
     $(".container-bottomlist").find(":first-child").remove().appendTo(".container-bottomlist");
     $(".container-bottomlist").find(":last-child").attr("style", "display:none");
     $(".container-bottomlist").children().eq(3).attr("style", "display:block")
