f5b4c8a220345438411231edbbc3f1dfe8b03ef7
deniszulic <35138382+deniszulic@users.noreply.github.com>
2021-11-24 10:07:14 +0100

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,7 @@ $("#container-first-button").click(() => {
   $("#container-first-button").attr("disabled", true);
   $("#container-second-button").attr("disabled", true);
   let width_top=$(".container-top").children().eq(4).width();
-  $(".container-top").children().eq(4).animate({width:-width_top}, 'slow',() => {
+  $(".container-top").children().eq(4).animate({width:width_top-width_top}, 'slow',() => {
     $(".container-top").find(":last-child").remove().prependTo(".container-top");
     $(".container-top").find(":first-child").attr("style", "display:block");
     $(".container-top").children().eq(5).attr("style", "display:none")
@@ -23,7 +23,7 @@ $("#container-first-button").click(() => {
   })
 
   let width_bottom=$(".container-bottomlist").children().eq(3).width();
-  $(".container-bottomlist").children().eq(3).animate({width:-width_bottom}, 'slow', () => {
+  $(".container-bottomlist").children().eq(3).animate({width:width_bottom-width_bottom}, 'slow', () => {
     $(".container-bottomlist").find(":last-child").remove().prependTo(".container-bottomlist");
     $(".container-bottomlist").find(":first-child").attr("style", "display:block");
     $(".container-bottomlist").children().eq(4).attr("style", "display:none")
